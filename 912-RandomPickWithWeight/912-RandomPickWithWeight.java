29abfeccb8104dcea9590c271ce94b1b0072a99f
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-11 19:43:52 +0530

    Create 912-RandomPickWithWeight

---
+++ 912-RandomPickWithWeight/912-RandomPickWithWeight.java
@@ -0,0 +1,36 @@
+// Last updated: 11/08/2026, 19:43:52
+class Solution {
+    private List<Integer> runningSums;
+    private int totalSum;
+
+    public Solution(int[] weights) {
+
+        runningSums = new ArrayList<>();
+        int runningSum = 0;
+
+        for (int w : weights) {
+            runningSum += w;
+            runningSums.add(runningSum);
+        }
+
+        totalSum = runningSum;
+    }
+
+    public int pickIndex() {
+        Random random = new Random();
+        int target = random.nextInt(totalSum) + 1;
+        int low = 0;
+        int high = runningSums.size();
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (target > runningSums.get(mid)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+}

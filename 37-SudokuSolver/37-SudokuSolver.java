7fac81fdc1e904fb5068ccddcd3f2c2ba4df3da8
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-11 14:17:56 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -0,0 +1,52 @@
+// Last updated: 11/08/2026, 14:17:56
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[row][col] = '.'; // backtrack
+                        }
+                    }
+
+                    return false; // no valid number found
+                }
+            }
+        }
+
+        return true; // board solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) return false;
+            if (board[i][col] == num) return false;
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int r = startRow; r < startRow + 3; r++) {
+            for (int c = startCol; c < startCol + 3; c++) {
+                if (board[r][c] == num) return false;
+            }
+        }
+
+        return true;
+    }
+}

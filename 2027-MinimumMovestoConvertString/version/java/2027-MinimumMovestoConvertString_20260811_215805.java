dd7f76e2e510c7a4c2b235a91c784b7779c19acc
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-11 21:58:06 +0530

    Create 2027-MinimumMovestoConvertString

---
+++ 2027-MinimumMovestoConvertString/version/java/2027-MinimumMovestoConvertString_20260811_215805.java
@@ -0,0 +1,16 @@
+// Last updated: 11/08/2026, 21:58:05
+1class Solution {
+2    public boolean containsNearbyDuplicate(int[] nums, int k) {
+3        Map<Integer, Integer> seen = new HashMap<>();
+4
+5        for (int i = 0; i < nums.length; i++) {
+6            int val = nums[i];
+7            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+8                return true;
+9            }
+10            seen.put(val, i);
+11        }
+12
+13        return false;        
+14    }
+15}

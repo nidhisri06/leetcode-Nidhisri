ab8837aa347a72b1db60c0bad1333980ea5ceab5
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-11 20:22:39 +0530

    Create 3251-MaximumAreaOfLongestDiagonalRectangle

---
+++ 3251-MaximumAreaOfLongestDiagonalRectangle/3251-MaximumAreaOfLongestDiagonalRectangle.java
@@ -0,0 +1,21 @@
+// Last updated: 11/08/2026, 20:22:39
+class Solution {
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        double maxi = -1.0;
+        int res = 0;
+        
+        for (int[] rect : dimensions) {
+            int l = rect[0], b = rect[1];
+            double d = Math.sqrt((double)l * l + (double)b * b);
+            
+            if (d > maxi) {
+                maxi = d;
+                res = l * b;
+            } else if (d == maxi) {
+                res = Math.max(res, l * b);
+            }
+        }
+        
+        return res;
+    }
+}

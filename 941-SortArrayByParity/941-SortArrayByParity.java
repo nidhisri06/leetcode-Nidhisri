d43cad629046fac05ce1a620f2abdbc5dc16865e
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-11 19:43:44 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.java
@@ -0,0 +1,28 @@
+// Last updated: 11/08/2026, 19:43:44
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        List<Integer> oddNumbers = new ArrayList<>();
+        
+        // Separate even and odd numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0)
+                evenNumbers.add(nums[i]);  // Even number found
+            else
+                oddNumbers.add(nums[i]);   // Odd number found
+        }
+        
+        // Combine even and odd numbers, placing even numbers first
+        for (int num : oddNumbers) {
+            evenNumbers.add(num);
+        }
+        
+        // Convert List to int[]
+        int[] result = new int[evenNumbers.size()];
+        for (int i = 0; i < evenNumbers.size(); i++) {
+            result[i] = evenNumbers.get(i);
+        }
+        
+        return result;  // Return the sorted array
+    }
+}

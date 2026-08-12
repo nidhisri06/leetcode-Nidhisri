447728d2c0fa0e8c5ba875e1676b4a9ac388b2fa
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-12 19:47:04 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,29 @@
+// Last updated: 12/08/2026, 19:47:03
+class MinStack {
+    private List<int[]> st;
+
+    public MinStack() {
+        st = new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        int[] top = st.isEmpty() ? new int[]{val, val} : st.get(st.size() - 1);
+        int min_val = top[1];
+        if (min_val > val) {
+            min_val = val;
+        }
+        st.add(new int[]{val, min_val});        
+    }
+    
+    public void pop() {
+        st.remove(st.size() - 1);
+    }
+    
+    public int top() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[0];
+    }
+    
+    public int getMin() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[1];
+    }
+}

c78dc8a96dc7567fd149e82c4a88392ec5750a3d
nidhisri06 <m.nidhisri06@gmail.com>
2026-08-12 15:15:06 +0530

    Create 744-FindSmallestLetterGreaterThanTarget

---
+++ 744-FindSmallestLetterGreaterThanTarget/version/java/744-FindSmallestLetterGreaterThanTarget_20260812_151506.java
@@ -0,0 +1,18 @@
+// Last updated: 12/08/2026, 15:15:06
+1class Solution {
+2    public List<Double> averageOfLevels(TreeNode root) {
+3        Queue<TreeNode> q = new LinkedList<>(List.of(root));
+4        List<Double> ans = new ArrayList<>();
+5        while (q.size() > 0) {
+6            double qlen = q.size(), row = 0;
+7            for (int i = 0; i < qlen; i++) {
+8                TreeNode curr = q.poll();
+9                row += curr.val;
+10                if (curr.left != null) q.offer(curr.left);
+11                if (curr.right != null) q.offer(curr.right);
+12            }
+13            ans.add(row/qlen);
+14        }
+15        return ans;
+16    }
+17}
